6ce60d6865c745cae4b8eeb561c8bfb272c070d9
Giovanni Graziano <giovanni.manolo.br@gmail.com>
2025-07-21 22:44:03 -0300

    fix: added a cooldow for attack

--- Scripts/Classes/actor.gd
+++ Scripts/Classes/actor.gd
@@ -2,9 +2,19 @@ class_name Actor extends Entity
 
 @export var speed: float = 0
 @export var strength: float = 0
+@export var cooldown: float = .5
+var cooldown_timer: Timer
 @export var attack_area: AttackArea
+var can_attack: bool = true
 
-func _init() -> void: motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+func _init() -> void:
+	
+	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+	var new_timer: Timer = Timer.new()
+	new_timer.wait_time = cooldown
+	new_timer.timeout.connect(_cooldown_timer_end)
+	cooldown_timer = new_timer
+	add_child(new_timer)
 
 func move(direction: Vector2) -> void:
 
@@ -12,11 +22,18 @@ func move(direction: Vector2) -> void:
 
 	move_and_collide(movement)
 
-func attack(direction: Vector2) -> void: 
+func attack(direction: Vector2) -> void:
 
-	attack_area.rotation = direction.angle()
+	if can_attack:
 
-	# Animation time not implemented yet
-	await get_tree().create_timer(0.1).timeout
+		can_attack = false
+		cooldown_timer.start()
 
-	attack_area.hit()
+		attack_area.rotation = direction.angle()
+
+		# Animation time not implemented yet
+		await get_tree().create_timer(0.1).timeout
+
+		attack_area.hit()
+
+func _cooldown_timer_end() -> void: can_attack = true
