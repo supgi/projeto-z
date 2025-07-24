bdbfc405bdf3bc44aea997953f71c9f51b7ab43d
Giovanni Graziano <giovanni.manolo.br@gmail.com>
2025-07-24 01:30:19 -0300

    feat: added heal and drop system

--- Scripts/Classes/entity.gd
+++ Scripts/Classes/entity.gd
@@ -1,11 +1,16 @@
 class_name Entity extends CharacterBody2D
 
 @export var health: float = 1
+var max_health: float = health
 @export var sprites: Node2D
+@export var drop: PackedScene
+@export_range(0.0, 10.0, 1.0) var quantity
 
 @export_group("configuration")
 @export var can_die: bool = true
 @export var can_hurt: bool = true
+
+signal is_dead
 
 func get_damage(value: float) -> void:
 
@@ -16,7 +21,13 @@ func get_damage(value: float) -> void:
 	
 	if can_die and health <= 0:
 
+		is_dead.emit()
 		_die()
+
+func heal(value: float) -> void:
+
+	health += value
+	if health > max_health: health = max_health
 
 func _hurt_effect() -> void:
 
@@ -26,4 +37,15 @@ func _hurt_effect() -> void:
 	tween.tween_property(sprites, "modulate", Color.RED, 0.05)
 	tween.tween_property(sprites, "modulate", Color.WHITE, 0.05).set_delay(0.25)
 
-func _die() -> void: queue_free()
+func _drop() -> void:
+
+	if drop:
+
+		var drop_instace := drop.instantiate()
+		drop_instace.global_position = global_position
+		get_parent().add_child(drop_instace)
+
+func _die() -> void:
+	
+	_drop()
+	queue_free()
